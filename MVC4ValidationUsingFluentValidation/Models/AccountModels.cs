5d27a5e11b88d57f95ee1431c4a7fc868d6f7738
Jerrie Pelser <jerrie.pelser@gmail.com>
2013-05-06 21:45:29 +0700

    Replace data annotations validation with Fluent Validation

---
+++ MVC4ValidationUsingFluentValidation/Models/AccountModels.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace MVC4ValidationUsingFluentValidation.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+    }
+
+    [FluentValidation.Attributes.Validator(typeof(RegisterExternalLoginModelValidator))]
+    public class RegisterExternalLoginModel
+    {
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    [FluentValidation.Attributes.Validator(typeof(LocalPasswordModelValidator))]
+    public class LocalPasswordModel
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    [FluentValidation.Attributes.Validator(typeof(LoginModelValidator))]
+    public class LoginModel
+    {
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    [FluentValidation.Attributes.Validator(typeof(RegisterModelValidator))]
+    public class RegisterModel
+    {
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
